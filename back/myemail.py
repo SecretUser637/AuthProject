49d81df90f57585bce7f630030809275b7b6b6c9
Your Name <you@example.com>
2024-04-26 15:21:27 +0500

    ch

--- back/myemail.py
+++ back/myemail.py
@@ -5,12 +5,12 @@ from config import settings
 def send_email_message(to,subject,text):
     message=MIMEText(text,"plain")
     message['Subject']=subject
-    message['From']="ИС регистрация"
+    message['From']=settings.EMAIL_LOG
     message['To']=to
     try:
         with smtplib.SMTP_SSL(settings.EMAIL_SMTP,settings.EMAIL_PORT) as server:
-            server.starttls()
+            # server.starttls()
             server.login(settings.EMAIL_LOG,settings.EMAIL_PWD)
             server.sendmail(settings.EMAIL_LOG,to,message.as_string())
-    except Exception:
-        print('Ошибка')
+    except Exception as e:
+        print(e)
