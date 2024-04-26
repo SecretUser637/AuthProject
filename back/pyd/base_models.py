7390061edb7af1fff6a11f4a074a68b6befb7e50
Your Name <you@example.com>
2024-04-26 14:18:33 +0500

    dateval

--- back/pyd/base_models.py
+++ back/pyd/base_models.py
@@ -5,9 +5,9 @@ class UserBase(BaseModel):
     id: int = Field(None,  example=1)
     email: EmailStr = Field(..., example='help@gmail.com')
     username:str=Field(...,example='nagibator69')
-    birthday:date=Field(...,example='01-01-2001')
+    birthday:date=Field(...,example='2001-01-01')
     email_verify:bool=Field(...)
-    created_at:datetime=Field(...,example='01-01-2001 00:00:00')
+    created_at:datetime=Field(...,example='2001-01-01 00:00:00')
 
     class Config:
         orm_mode=True
--- back/pyd/create_models.py
+++ back/pyd/create_models.py
@@ -6,4 +6,4 @@ class UserCreate(BaseModel):
     email: EmailStr = Field(..., example='help@gmail.com')
     username:str=Field(...,max_length=255,min_length=3,example='nagibator69')
     pwd:str=Field(...,max_length=255,min_length=6,example='123456')
-    birthday:date=Field(...,example='01-01-2001')
+    birthday:date=Field(...,example='2001-01-01')
