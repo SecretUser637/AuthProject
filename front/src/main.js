0a193fee34010279dd0f2d773ad48fec8d7c8de3
Your Name <you@example.com>
2024-05-11 14:27:54 +0500

    add axios

--- front/src/main.js
+++ front/src/main.js
@@ -1,11 +1,28 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 const app = createApp(App)
 
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+app.use(VueAxios, axios)
+
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
--- front/src/views/RegisterView.vue
+++ front/src/views/RegisterView.vue
@@ -1,10 +1,48 @@
 <template>
     <div>
     <h1>Регистрация</h1>
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="user.username"
+        label="Логин"
+      ></v-text-field>
+      <v-text-field
+        v-model="user.email"
+        label="Email"
+      ></v-text-field>
+      <v-text-field
+        v-model="user.pwd"
+        label="Пароль"
+      ></v-text-field>
+      <v-text-field
+        v-model="user.pwd"
+        label="Пароль"
+      ></v-text-field>
+      <v-text-field
+        v-model="user.birthday"
+        label="День рождения"
+      ></v-text-field>
+      <v-btn class="mt-2" type="submit" block @click="send">Регистрация</v-btn>
+    </v-form>
     </div>
 </template>
 
 
 <script>
+ export default {
+    methods:{
+        send(){
+            console.log(1)
+        }
+    },
+    data: () => ({
+      user:{
+        username:null,
+        email:null,
+        pwd:null,
+        birthday:null
+      }
+    }),
+ }
 </script>
 
